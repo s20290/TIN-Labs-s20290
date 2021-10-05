5148aa71239974d2887d7d96165c2afcd90966fe
s20290 <s20290@pjwstk.edu.pl>
2021-10-05 18:12:58 +0200

    fixing package

---
+++ Lab7/7b.js
@@ -0,0 +1,19 @@
+const filename = "D:\\pjatk\\3rd year\\5thSemester\\TIN\\labs\\Lab7"
+// const filename = ".\\sup.txt" 
+const { watch } = require('fs/promises');
+
+const ac = new AbortController();
+const { signal } = ac;
+setTimeout(() => ac.abort(), 20000);
+
+(async () => {
+  try {
+    const watcher = watch(filename, { signal });
+    for await (const event of watcher)
+      console.log(event);
+  } catch (err) {
+    if (err.name === 'AbortError')
+      return;
+    throw err;
+  }
+})();
