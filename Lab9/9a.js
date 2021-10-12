2f572de368b877f66fa9f723c2d63a0499e8bcc3
Alexander Tu <s20290@pjwstk.edu.pl>
2021-10-12 10:01:09 +0200

    commit

---
+++ Lab9/9a.js
@@ -0,0 +1,11 @@
+document.getElementById('mybtn').addEventListener('click',getJsonData)
+
+function getJsonData(){
+    var xhr = new XMLHttpRequest()
+    xhr.open("GET", "9a.json",true)
+
+    
+    if(this.stauts == 200){
+
+    }
+}
